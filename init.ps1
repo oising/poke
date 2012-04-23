c2981bd82318b1218bf538682debe5defeece713
oising <oising@localhost>
2012-04-22 23:55:25 -0400

    added partial modifier get-member output implementation - properties/fields don't show correct modifier yet.

--- init.ps1
+++ init.ps1
@@ -1,13 +1,5 @@
-﻿
+﻿# We run this outside of module scope in caller scope.
+# We cannot use the manifest (PSD1) as it cannot override
+# core formats because it uses -AppendPath semantics.
 
 update-formatdata -PrependPath (join-path $psscriptroot 'Poke.Format.ps1xml')
-
-<#
-Update-TypeData -typename Microsoft.PowerShell.Commands.MemberDefinition -MemberType ScriptProperty -MemberName MemberType -Value {    
-    try { invoke-formathelper $this get-membertype } catch { write-warning "oops: $_" }
-} -Force
-
-Update-TypeData -typename Microsoft.PowerShell.Commands.MemberDefinition -MemberType ScriptProperty -MemberName Modifier -Value {    
-    try { invoke-formathelper $this get-membermodifier } catch { write-warning "oops: $_" }
-} -Force
-#>
