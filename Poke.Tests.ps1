0caa68ff5f2510bd9fb9a0f0d21a96fbc17ea4d9
Sergei Vorobev <sevoroby@microsoft.com>
2015-10-22 20:07:21 -0700

    Remvoe unnessary Add-Type from test

--- Poke.Tests.ps1
+++ Poke.Tests.ps1
@@ -4,18 +4,8 @@ Describe 'Poke' {
     Context 'regression tests' {
         # https://github.com/oising/poke/issues/4 
         It 'can poke a class with a method conflicting with a function' {
-            function MMM() {}
-
-            Add-Type @'
-namespace NNN
-{
-    public class CCC
-    {
-        private static void MMM() {}
-    }
-}
-'@
-            [NNN.CCC] | peek | Should Not Be $null
+            function Contains() {}
+            'abc' | peek | Should Not Be $null
         }
     }
 }
