430ba2ecaa84bfe1707db8976fe7b23f1c49b698
Oisin Grehan <oising@gmail.com>
2016-02-06 15:44:16 -0500

    renamed init.ps1 to poke_init.ps1; added oneremove to unload init after poke unload; added psd1 metadata

---
+++ poke_init.ps1
@@ -0,0 +1,5 @@
+﻿# We run this outside of module scope in caller scope.
+# We cannot use the manifest (PSD1) as it cannot override
+# core formats because it uses -AppendPath semantics.
+
+update-formatdata -PrependPath (join-path $psscriptroot 'Poke.Format.ps1xml')
